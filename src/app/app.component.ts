8e941196a9485b666da0a795abed69ec40478ca0
Yura Kochut <kochutyurawork@gmail.com>
2020-05-03 15:58:43 +0300

    add admin page

---
+++ src/app/app.component.ts
@@ -0,0 +1,52 @@
+import { Component, ViewEncapsulation, OnInit, ElementRef, ViewChild, Renderer2, HostListener } from '@angular/core';
+import * as AOS from '../../node_modules/aos';
+import { AdminService } from './shared/services/admin.service';
+import { FireService } from './shared/services/fire.service';
+import { ToastrService } from 'ngx-toastr';
+import { WindowService } from './shared/services/window.service';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class AppComponent implements OnInit {
+  users: any[] = [];
+  title = 'app';
+
+  @ViewChild('ContentWidth', { static: false }) ContentWidth: ElementRef; // get navbar
+
+  constructor(
+    public r: Renderer2,
+    public AdminService: AdminService,
+    public FireService: FireService,
+    public toast: ToastrService,
+    private windowS: WindowService,
+    private router: Router
+  ) {
+
+  }
+  test() {
+    this.toast.success("I'm a toast!", "Success!");
+  }
+
+  ngOnInit() {
+    AOS.init({
+      offset: 200,
+      duration: 700,
+      easing: 'linear'
+    });
+    this.windowS.width = window.innerWidth
+    this.windowS.height = window.innerHeight;
+    this.windowS.scroll = window.pageYOffset;
+  }
+
+  @HostListener('window:resize', ['$event']) onResize(event) {
+    this.windowS.width = event.target.innerWidth
+    this.windowS.height = event.target.innerHeight;
+    
+  }
+
+
+}
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit, HostListener, ViewChild, ElementRef } from '@angular/core';
+
+import { trigger, transition, useAnimation } from '@angular/animations';
+import { bounce, fadeInLeft } from 'ng-animate';
+import { WindowService } from 'src/app/shared/services/window.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+  animations: [
+    trigger('bounce', [transition('* => *', useAnimation(bounce))]),
+    trigger('fadeInLeft', [transition('* => *', useAnimation(fadeInLeft))]),
+  ],
+})
+export class HomeComponent implements OnInit {
+  bounce: any;
+  fadeInLeft: any;
+  @ViewChild('portfolio', { static: false }) portfolio: ElementRef;
+  @ViewChild('about', { static: false }) about: ElementRef;
+  @ViewChild('contact', { static: false }) contact: ElementRef;
+  constructor(
+    private windowS: WindowService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+  }
+
+  @HostListener('window:scroll', ['$event']) onScroll(event) {
+    this.getNavigate();
+
+  }
+
+  getNavigate(): void{
+    const portfolioTop: number = this.portfolio.nativeElement.getBoundingClientRect().top;
+    const aboutTop: number = this.about.nativeElement.getBoundingClientRect().top;
+    const contactTop: number = this.contact.nativeElement.getBoundingClientRect().top;
+    this.windowS.scroll = window.pageYOffset;
+    if (portfolioTop >= 300) {
+      this.router.navigate(['/home']);
+    }
+    if (portfolioTop < 0) {
+      this.router.navigate(['/portfolio']);
+    }
+
+    if (aboutTop < 0) {
+      this.router.navigate(['/about']);
+    }
+
+    if (contactTop < 0) {
+      this.router.navigate(['/contact']);
+    }
+  }
+
+}
